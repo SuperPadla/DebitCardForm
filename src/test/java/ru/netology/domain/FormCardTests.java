655436c4bbf0aee5a8d479f3e2605a43f3f3dee5
Ксения Сажнева <13pokemon@mail.ru>
2022-10-03 19:03:03 +0300

    fixed the tests

--- src/test/java/ru/netology/domain/FormCardTests.java
+++ src/test/java/ru/netology/domain/FormCardTests.java
@@ -29,7 +29,7 @@ public class FormCardTests {
         $("[data-test-id = phone] input").setValue("+79336657088");
         $("[data-test-id = agreement]").click();
         $("button[type=button]").click();
-        $("[data-test-id = name] .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
     @Test
     void wrongPhone() {
@@ -37,15 +37,24 @@ public class FormCardTests {
         $("[data-test-id = phone] input").setValue("89336657088");
         $("[data-test-id = agreement]").click();
         $("button[type=button]").click();
-        $("[data-test-id = phone] .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id = phone].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
     @Test
-    void emptyField() {
+    void emptyName() {
         $("[data-test-id = name] input").setValue("");
         $("[data-test-id = phone] input").setValue("+79336657088");
         $("[data-test-id = agreement]").click();
         $("button[type=button]").click();
-        $("[data-test-id = name] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id = name].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+    //Необходимо также проверить форму с пустым полем ввода номера телефона, пропущенный тест прошу добавить.
+    @Test
+    void emptyPhone() {
+        $("[data-test-id = name] input").setValue("Биба Бобов");
+        $("[data-test-id = phone] input").setValue("");
+        $("[data-test-id = agreement]").click();
+        $("button[type=button]").click();
+        $("[data-test-id = phone].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
     @Test
     void shouldFirstErrorField() {
@@ -53,25 +62,19 @@ public class FormCardTests {
         $("[data-test-id = phone] input").setValue("89336657088");
         $("[data-test-id = agreement]").click();
         $("button[type=button]").click();
-        $("[data-test-id = name]").shouldHave(Condition.cssClass("input_invalid"));
-        // или так:
-        // $("[data-test-id = name] .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id = name].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
     @Test
     void shouldFirstErrorField1() {
         $("[data-test-id = name] input").setValue("Биба Бобов");
         $("[data-test-id = phone] input").setValue("89336657088");
-        //специально не кликаю чекбокс, чтобы просмотреть очередность ошибок
         $("button[type=button]").click();
-        $("[data-test-id = phone]").shouldHave(Condition.cssClass("input_invalid"));
-        //или так:
-        // $("[data-test-id = phone] .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id = phone].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
     @Test
     void invalidCheckBox() {
         $("[data-test-id = name] input").setValue("Биба Бобов");
         $("[data-test-id = phone] input").setValue("+79336657088");
-        //пропускаем чекбокс, по умолчанию он выключен
         $("button[type=button]").click();
         $("[data-test-id = agreement]").shouldHave(Condition.cssClass("input_invalid"));
     }
